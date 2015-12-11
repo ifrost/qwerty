4e4913aa0b903f19d6919044f149a2f7b47eb513
Piotr Jamroz <pm.jamroz@gmail.com>
2015-12-11 01:21:29 +0000

    add a simple html example

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 var gulp  = require('gulp');
 var shell = require('gulp-shell');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
 
 gulp.task('doc', shell.task('jsdoc -c jsdoc.conf.json'));
 gulp.task('test', shell.task('mocha --recursive'));
 gulp.task('dtest', shell.task('mocha --recursive --debug'));
 gulp.task('coverage', shell.task('istanbul cover node_modules/mocha/bin/_mocha -- --reporter spec --recursive'));
+
+gulp.task('browserify', function() {
+    // Single entry point to browserify
+    gulp.src('index.js')
+        .pipe(browserify({
+            insertGlobals : false,
+            debug : false
+        }))
+        .pipe(rename('qwerty.js'))
+        .pipe(gulp.dest('./dist'))
+});
---
+++ index.js
@@ -0,0 +1,8 @@
+var TextSuite = require('./lib/suite/textsuite');
+
+var Qwerty = {
+    TextSuite: TextSuite
+};
+
+global.Qwerty = Qwerty;
+module.exports = Qwerty
